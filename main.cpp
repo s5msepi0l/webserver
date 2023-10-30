b1d6336dffcd8c3c02bbec8f3bcff6f46d7a917c
jacob lindborg <65565795+s5msepi0l@users.noreply.github.com>
2023-10-30 23:20:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
+#define LOGGER_ENABLE_AUTO_WRITE 64 // Not neccesary at all, should probably be turned off in prod
+
 #include "src/webserver.h" 
 #include "src/logging.h"
 
 int main(int argc, const char **argv) {
+	http::webserver server(
+		"/home/s5msep1ol/Desktop/projects/webserver/backup_1/site", 
+		"/home/s5msep1ol/Desktop/projects/webserver/backup_1/logs",
+		8004, 
+		512, 
+		4);
     
-	http::webserver server("/home/s5msep1ol/Desktop/projects/webserver/main/site", 8004, 512, 4);
-    server.get("/", [](parsed_request& request, packet_response& response) {
+	server.get("/", [](parsed_request& request, packet_response& response) {
 		set_body_content("index.html", response);
 		set_content_type("text/html", response);
 		set_content_status(200, response);
 
 	});
 
-
     sleep(120);
 
     server.shutdown();
-    
+
 	return 0;
 }
