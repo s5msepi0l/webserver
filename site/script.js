7816c6f0ac7b7ae9d1f0572a1cd2b24ee33b3713
jacob lindborg <65565795+s5msepi0l@users.noreply.github.com>
2023-11-10 02:22:32 +0200

    Update script.js

--- site/script.js
+++ site/script.js
@@ -1,10 +1,12 @@
-async function send_http_request() {
-    try {
-      const url = '127.0.0.1:8082/num'; // Replace with your desired URL
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log('Output from HTTP request:', data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+const login_btn = document.getElementById("Login");
+
+login_btn.addEventListener("click", async function() {
+  try {
+    const url = '/num'; // Add 'http://' before the URL
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log('Output from HTTP request:', data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
   }
+});
